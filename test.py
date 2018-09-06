9e1a764bcd070d0f1eed646231d08612b609ae56
root <root@localhost.localdomain>
2018-09-06 09:25:49 -0500

    test

--- test.py
+++ test.py
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
 print "What up"
-=======
-<<<<<<< HEAD
 print "What up" 
->>>>>>> 632c1ddc56fedca15e813abbaf1584e60848a92e
 print "yo!fuckoff"
-=======
-print "yo!"
->>>>>>> parent of c76b24b... Salut jai modifié les fichiers mofo
-
-
 print "test"
